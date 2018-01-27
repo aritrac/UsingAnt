ebac38bb3c447bf51a82e186f38bdf547f688106
aritrac <aritrachatterjee2007@gmail.com>
2018-01-27 21:15:24 +0530

    Refactored existing projects

---
+++ JavadocExamples/src/com/aritra/examples/GenerateDoc.java
@@ -0,0 +1,20 @@
+package com.aritra.examples;
+
+/**
+ * 
+ * @author Aritra Chatterjee
+ * @version 1.0
+ *
+ */
+public class GenerateDoc {
+	/**
+	 * 
+	 * @param args This will accept command line arguments
+	 * @author Aritra Chatterjee
+	 * @return Does not return anything
+	 * @throws No exception is thrown
+	 */
+	public static void main(String[] args) {
+		System.out.println("Java doc will be generated for this method");
+	}
+}
---
+++ JavadocExamples/src/com/aritra/examples/exclude/ExcludeMe.java
@@ -0,0 +1,20 @@
+package com.aritra.examples.exclude;
+
+/**
+ * 
+ * @author Aritra Chatterjee
+ * @version 1.1
+ *
+ */
+public class ExcludeMe {
+	/**
+	 * 
+	 * @param args This will accept command line arguments
+	 * @author Aritra Chatterjee
+	 * @return Does not return anything
+	 * @throws No exception is thrown
+	 */
+	public static void main(String[] args) {
+		System.out.println("This will be excluded from doc generation");
+	}
+}
