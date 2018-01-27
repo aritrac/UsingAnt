a31e79da06f8133cedc574856cd906d10b49af50
aritrac <aritrachatterjee2007@gmail.com>
2018-01-27 21:32:57 +0530

    Added javadoc generation

--- JavadocExamples/src/com/aritra/examples/GenerateDoc.java
+++ JavadocExamples/src/com/aritra/examples/GenerateDoc.java
@@ -11,8 +11,6 @@ public class GenerateDoc {
 	 * 
 	 * @param args This will accept command line arguments
 	 * @author Aritra Chatterjee
-	 * @return Does not return anything
-	 * @throws No exception is thrown
 	 */
 	public static void main(String[] args) {
 		System.out.println("Java doc will be generated for this method");
--- JavadocExamples/src/com/aritra/examples/exclude/ExcludeMe.java
+++ JavadocExamples/src/com/aritra/examples/exclude/ExcludeMe.java
@@ -11,8 +11,6 @@ public class ExcludeMe {
 	 * 
 	 * @param args This will accept command line arguments
 	 * @author Aritra Chatterjee
-	 * @return Does not return anything
-	 * @throws No exception is thrown
 	 */
 	public static void main(String[] args) {
 		System.out.println("This will be excluded from doc generation");
