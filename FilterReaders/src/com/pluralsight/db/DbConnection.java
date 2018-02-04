99c44ddc98cf2a759c6c7849af04831c0ec6f98f
aritrac <aritrachatterjee2007@gmail.com>
2018-02-04 10:40:06 +0530

    Added filtereaders

---
+++ FilterReaders/src/com/pluralsight/db/DbConnection.java
@@ -0,0 +1,30 @@
+package com.pluralsight.db;
+
+/*
+ * Handles all connections to the database
+ * @author Aritra
+ * @version 1.0
+ * @TODAY@
+ */
+
+/**
+ * Establishes a connection to the database
+ * @author Rusty
+ * @version 1.0
+ * @TODAY@
+ */
+
+public final class DbConnection {
+	/**
+	 * Establishes a connection to the database
+	 * @author Aritra
+	 */
+	public static final String number1= "29";
+	public static final String number2= "29";
+	public static final String number3= "29";
+	public static final String number4= "29";
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ FilterReaders/src/sample.sql
@@ -0,0 +1,7 @@
+--sample.sql
+--
+--Note: if geometries do not span more than 1 row, you can remove
+--the DISTINCT qualifier from the select statement
+--
+
+Select distict sdogid from POLYGON_SDOGEOM;
